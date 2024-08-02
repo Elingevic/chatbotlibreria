9fb109fba871e5c7074f4f23d3e61889d81d515b
Elingevic <victorcastorani@gmail.com>
2024-08-02 17:13:50 -0600

    Agregué nuevas funcionalidades al chatbot

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
-
-const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
